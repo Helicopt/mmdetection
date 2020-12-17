e1b40ddb7f52eb332c7e12f92f8365ac444e3f6f
Helicopt <fwtt20071028@126.com>
2020-12-17 20:59:31 +1100

    custom script

--- tools/custom_test.sh
+++ tools/custom_test.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-set -x
+# set -x
 
 . tools/env.sh
 
 PARTITION=$1
-JOB_NAME=$2
-CONFIG=$3
-CHECKPOINT=$4
-GPUS=${GPUS:-8}
-GPUS_PER_NODE=${GPUS_PER_NODE:-8}
-CPUS_PER_TASK=${CPUS_PER_TASK:-5}
-PY_ARGS=${@:5}
+JOB_NAME=dgs
+CONFIG=$2
+CHECKPOINT=$3
+GPUS=8
+GPUS_PER_NODE=8
+CPUS_PER_TASK=3
+PY_ARGS=${@:4}
 SRUN_ARGS=${SRUN_ARGS:-""}
 
 srun -p ${PARTITION} \
--- tools/custom_train.sh
+++ tools/custom_train.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-set -x
+# set -x
 
 . tools/env.sh
 
 PARTITION=$1
-JOB_NAME=$2
-CONFIG=$3
-WORK_DIR=$4
-GPUS=${GPUS:-8}
-GPUS_PER_NODE=${GPUS_PER_NODE:-8}
-CPUS_PER_TASK=${CPUS_PER_TASK:-5}
+JOB_NAME=dgs
+CONFIG=$2
+GPUS=8
+GPUS_PER_NODE=8
+CPUS_PER_TASK=3
 SRUN_ARGS=${SRUN_ARGS:-""}
-PY_ARGS=${@:5}
+PY_ARGS=${@:3}
 
 srun -p ${PARTITION} \
     --job-name=${JOB_NAME} \
@@ -22,4 +21,4 @@ srun -p ${PARTITION} \
     --cpus-per-task=${CPUS_PER_TASK} \
     --kill-on-bad-exit=1 \
     ${SRUN_ARGS} \
-    python -u tools/train.py ${CONFIG} --work-dir=${WORK_DIR} --launcher="slurm" ${PY_ARGS}
+    python -u tools/train.py ${CONFIG} --launcher="slurm" ${PY_ARGS}
